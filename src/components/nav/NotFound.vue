4d00552accbd40d4e1c28b12ef5fc1e725881a38
Thomas Svenson <thomas@tsvenson.com>
2021-02-20 10:03:51 +0100

    Problem with that 'inject' diplays as errors for 'provide' data i VS Code on Ubuntu 20.04. In this archive it does so of the TeamsList.vue and UsersLists.vue components.

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>Page not found!</h2>
+  <p>Maybe view our <router-link to="/teams">Teams</router-link>?</p>
+</template>
---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,37 @@
+<template>
+<button @click="confirmInput">Confirm</button>
+  <ul>
+    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  methods: {
+    confirmInput() {
+      // do something
+      this.$router.push('/teams');
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('UserList cCmp beforeRouteEnter');
+    console.log(to, from);
+    next();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
